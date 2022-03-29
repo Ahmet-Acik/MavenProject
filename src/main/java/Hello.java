d8459612fac2beb8530ceb36063146667065d214
Ahmet-Acik <a.acik@icloud.com>
2022-03-29 17:21:08 +0100

    more work to GitGub

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Hello {
 
     public static void main(String[] args) {
@@ -6,5 +8,19 @@ public class Hello {
         System.out.println("Developing Hello feature");
         System.out.println("Developing Hello feature");
         System.out.println("Local repo is already has published to GitHub");
+
+
+
+        String[] array = {"A", "B", "C", "D", "E"};
+
+        for (int i = 0; i < array.length / 2; i++) {
+            String temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+
+
+
+        System.out.println(Arrays.toString(array));
     }
 }
